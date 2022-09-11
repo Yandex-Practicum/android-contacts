0e3fc8650aaaefa9bdfeba62509b952859858afa
dmitr <dmitryoreskin@gmail.com>
2022-09-11 15:21:28 +0300

    Проектное работа № 2, попытка № 1 (Позябин)

---
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.contacts.presentation.base;
+
+public interface ListDiffInterface <T> {
+
+    public boolean theSameAs(T obj);
+    public boolean equals(Object obj);
+
+}
