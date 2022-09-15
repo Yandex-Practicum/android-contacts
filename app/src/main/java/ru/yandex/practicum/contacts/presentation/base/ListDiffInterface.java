eeaa94d6725fefe3b030de972c5c93adf7305944
mvrlrd <mvrlrd@ya.ru>
2022-09-15 12:33:38 +0300

    добавил методы theSameAs & equals в ListDiffInterface

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
@@ -1,4 +1,6 @@
 package ru.yandex.practicum.contacts.presentation.base;
 
-public interface ListDiffInterface {
+public interface ListDiffInterface<T> {
+    boolean theSameAs(T t);
+    boolean equals(Object o);
 }
