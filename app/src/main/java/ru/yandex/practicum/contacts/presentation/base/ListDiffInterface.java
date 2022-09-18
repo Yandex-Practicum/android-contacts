fbf2a008ce6c6bc71431a500ac0b8f7b9c638bb3
Skankhunt68 <leono-dim1@yandex.ru>
2022-09-18 13:19:00 +0300

    add interface ListDiffInterface and Generic class BaseListDiffCallback

---
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.contacts.presentation.base;
+
+import ru.yandex.practicum.contacts.presentation.main.ContactUi;
+
+public interface ListDiffInterface<T> {
+    boolean theSameAs(T t);
+    boolean equals(Object object);
+}
