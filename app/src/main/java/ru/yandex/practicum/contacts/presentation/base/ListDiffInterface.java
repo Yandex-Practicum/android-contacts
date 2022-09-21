731faceae8b810eb82bd80c131f324cffa52d516
kotzimaru <kotzimaru@yandex.ru>
2022-09-21 18:40:51 +0300

    Release #1

---
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.contacts.presentation.base;
+
+public interface ListDiffInterface<T> {
+
+    public boolean theSameAs(T t);
+
+    public boolean equals(Object o);
+
+
+
+
+
+}
