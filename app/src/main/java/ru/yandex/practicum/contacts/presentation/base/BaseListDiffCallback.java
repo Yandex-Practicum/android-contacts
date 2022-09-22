0b5350318a4537da6a462eaa293300d05b3234d1
kotzimaru <kotzimaru@yandex.ru>
2022-09-22 15:25:18 +0300

    Release #1

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -1,9 +1,11 @@
 package ru.yandex.practicum.contacts.presentation.base;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.DiffUtil;
 
 public class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUtil.ItemCallback<T>{
+
     @Override
     public boolean areItemsTheSame(@NonNull T oldItem, @NonNull T newItem) {
 
@@ -13,5 +15,10 @@ public class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUt
     @Override
     public boolean areContentsTheSame(@NonNull T oldItem, @NonNull T newItem) {
         return oldItem.equals(newItem);
+    }
+    @Nullable
+    @Override
+    public Object getChangePayload(@NonNull T oldItem, @NonNull T newItem) {
+        return newItem;
     }
 }
--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
@@ -2,9 +2,9 @@ package ru.yandex.practicum.contacts.presentation.base;
 
 public interface ListDiffInterface<T> {
 
-    public boolean theSameAs(T t);
+    boolean theSameAs(T t);
 
-    public boolean equals(Object o);
+    boolean equals(Object o);
 
 
 
