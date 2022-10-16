22670fcb20ce0438da5e79095e1d9812b301c5c5
Oleg <olegkostinds@gmail.com>
2022-10-16 19:05:01 +0300

    fourth step

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -1,9 +1,15 @@
 package ru.yandex.practicum.contacts.presentation.base;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.DiffUtil;
 
-public class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUtil.ItemCallback<T>{
+public class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUtil.ItemCallback<T> {
+    @Nullable
+    @Override
+    public Object getChangePayload(@NonNull T oldItem, @NonNull T newItem) {
+        return newItem;
+    }
 
     @Override
     public boolean areItemsTheSame(@NonNull T oldItem, @NonNull T newItem) {
