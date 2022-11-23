c931551aede2ecfc08a7d8c5e640fd756b636350
anonim-no <anonimno@gmail.com>
2022-11-23 15:19:55 +1100

    Исправление ошибок по ревью - передаем дженерик в ListDiffInterface<T> - добавлен потеряный метод getChangePayload()

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -1,9 +1,10 @@
 package ru.yandex.practicum.contacts.presentation.base;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.recyclerview.widget.DiffUtil;
 
-public class BaseListDiffCallback<T extends ListDiffInterface> extends DiffUtil.ItemCallback<T> {
+public class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUtil.ItemCallback<T> {
 
     @Override
     public boolean areItemsTheSame(@NonNull T oldItem, @NonNull T newItem) {
@@ -13,5 +14,11 @@ public class BaseListDiffCallback<T extends ListDiffInterface> extends DiffUtil.
     @Override
     public boolean areContentsTheSame(@NonNull T oldItem, @NonNull T newItem) {
         return oldItem.equals(newItem);
+    }
+
+    @Nullable
+    @Override
+    public Object getChangePayload(@NonNull T oldItem, @NonNull T newItem) {
+        return newItem;
     }
 }
