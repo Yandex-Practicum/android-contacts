31c2abdddeab74fe8bc0aff73e111b0c6dfe7c5e
JudjinGM <judjingm@gmail.com>
2022-11-29 22:37:04 +0300

    went through project work of spring 4

---
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
@@ -0,0 +1,8 @@
+package ru.yandex.practicum.contacts.presentation.base;
+
+import java.util.Objects;
+
+public interface ListDiffInterface<T> {
+    boolean theSameAs(T t);
+    boolean equals(Object o);
+}
