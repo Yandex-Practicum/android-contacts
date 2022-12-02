3a56e4cad6607dabd965dcfef899f639fb28bf86
Konstantin <Memoarikhizuka>
2022-12-02 22:29:27 +0800

    Finish this leasson

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/main/ContactUi.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/main/ContactUi.java
@@ -5,8 +5,10 @@ import androidx.annotation.NonNull;
 import java.util.List;
 
 import ru.yandex.practicum.contacts.model.ContactType;
+import ru.yandex.practicum.contacts.presentation.base.ListDiffInterface;
+import ru.yandex.practicum.contacts.presentation.sort.SortTypeUI;
 
-public class ContactUi {
+public class ContactUi implements ListDiffInterface<ContactUi> {
 
     private final String name;
     private final String phone;
@@ -39,6 +41,16 @@ public class ContactUi {
 
     public List<ContactType> getTypes() {
         return types;
+    }
+
+    /*@Override
+    public boolean theSameAs(contactUi sortTypeUI) {
+        return false;
+    }*/
+
+    @Override
+    public boolean theSameAs(ContactUi contactUi) {
+        return this.hashCode()==contactUi.hashCode();
     }
 
     @Override
