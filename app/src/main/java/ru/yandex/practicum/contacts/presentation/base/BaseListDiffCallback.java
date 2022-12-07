d9032df4383c74ef4577d37998b08d913120f102
questoedonista <sred75@yandex.ru>
2022-12-07 12:32:56 +0300

    спринт 4, задача на дженерики, исправил areItemsTheSame

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -8,7 +8,8 @@ public class BaseListDiffCallback<T extends ListDiffInterface<T> > extends ItemC
 
     @Override
     public boolean areItemsTheSame(@NonNull T oldItem, @NonNull T newItem) {
-        return oldItem.theSameAs(oldItem) == newItem.theSameAs(newItem);
+        //return oldItem.theSameAs(oldItem) == newItem.theSameAs(newItem);
+        return newItem.theSameAs(newItem);
     }
 
     @Override
