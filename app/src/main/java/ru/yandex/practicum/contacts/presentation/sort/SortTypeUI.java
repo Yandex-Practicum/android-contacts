d940b4ee0e2aef60dde1d678e7a46b61314e5f1d
DFater <118625726+DFater@users.noreply.github.com>
2022-12-24 17:18:20 +0300

    Первый коммит Практической работы №2, работа оказалась проще, чем думал, надеюсь сделал верно! :)

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortTypeUI.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortTypeUI.java
@@ -2,10 +2,12 @@ package ru.yandex.practicum.contacts.presentation.sort;
 
 import androidx.annotation.NonNull;
 
+import ru.yandex.practicum.contacts.presentation.base.ListDiffInterface;
+import ru.yandex.practicum.contacts.presentation.main.ContactUi;
 import ru.yandex.practicum.contacts.presentation.sort.model.SortType;
 
-public class SortTypeUI {
 
+public class SortTypeUI implements ListDiffInterface<SortTypeUI> {
     private final SortType sortType;
     private final boolean selected;
 
@@ -23,6 +25,11 @@ public class SortTypeUI {
     }
 
     @Override
+    public boolean theSameAs(SortTypeUI sortTypeUI) {
+        return this.hashCode() == sortTypeUI.hashCode();
+    }
+
+        @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
