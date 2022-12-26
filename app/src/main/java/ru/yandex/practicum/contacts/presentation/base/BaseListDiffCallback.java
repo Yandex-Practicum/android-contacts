847068d481bd415978b370fbee8e0abe54efc804
linyxoid <as@63ru.net>
2022-12-26 14:39:00 +0400

    Compare fixed

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -7,7 +7,7 @@ import androidx.recyclerview.widget.DiffUtil;
 public class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUtil.ItemCallback<T> {
     @Override
     public boolean areItemsTheSame(@NonNull T oldItem, @NonNull T newItem) {
-        return oldItem.hashCode() == newItem.hashCode();
+        return oldItem.theSameAs(newItem);
     }
 
     @Override
@@ -15,7 +15,7 @@ public class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUt
         return oldItem.equals(newItem);
     }
 
-    @Nullable
+    @NonNull
     @Override
     public Object getChangePayload(@NonNull T oldItem, @NonNull T newItem) {
         return newItem;
