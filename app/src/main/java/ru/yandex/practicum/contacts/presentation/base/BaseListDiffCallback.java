7cce268fb9ae8dbd10dd37ad879c6c689e10bcc1
ZaharinVN <79045399863@yandex.ru>
2023-03-12 22:37:37 +0300

    ProjectWork_2

---
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.contacts.presentation.base;
+
+public class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUtil.ItemCallback<T> {
+
+    @Override
+    public boolean areItemsTheSame(@NonNull T oldItem, @NonNull T newItem) {
+        return oldItem.theSameAs(newItem);
+    }
+
+    @Override
+    public boolean areContentsTheSame(@NonNull T oldItem, @NonNull T newItem) {
+        return false;
+    }
+}
