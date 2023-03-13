2a6867e3f88d816bc4c12985693bcdca96897bda
SomnyMosh <124548508+SomnyMosh@users.noreply.github.com>
2023-03-13 15:06:44 +0800

    Add files via upload

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/filter/model/FilterContactTypeUi.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/filter/model/FilterContactTypeUi.java
@@ -2,35 +2,36 @@ package ru.yandex.practicum.contacts.presentation.filter.model;
 
 import androidx.annotation.NonNull;
 
-public class FilterContactTypeUi {
+import ru.yandex.practicum.contacts.presentation.base.ListDiffInterface;
+
+public class FilterContactTypeUi implements ListDiffInterface<FilterContactTypeUi> {
 
     private final FilterContactType contactType;
     private final boolean selected;
-
     public FilterContactTypeUi(@NonNull FilterContactType contactType, boolean selected) {
         this.contactType = contactType;
         this.selected = selected;
     }
-
     public FilterContactType getContactType() {
         return contactType;
     }
-
     public boolean isSelected() {
         return selected;
+    }
+
+    @Override
+    public boolean theSameAs(FilterContactTypeUi t) {
+        return this.getContactType() == t.getContactType();
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-
         FilterContactTypeUi that = (FilterContactTypeUi) o;
-
         if (selected != that.selected) return false;
         return contactType == that.contactType;
     }
-
     @Override
     public int hashCode() {
         int result = contactType.hashCode();
