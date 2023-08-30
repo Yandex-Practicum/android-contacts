3cd9ac79c97e68ca0d6e1922543f1e928f9750ed
falconArdente <iwsokolov@yandex.ru>
2023-08-30 13:24:38 +0300

    Перепроверена реализация методов сравнения внутри классов, реализующих интерфейс; Созданы объекты класса BaseListDiffCallback в адаптерах, из адаптеров удалены реализации предыдущего ддиффера;

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -2,10 +2,11 @@ package ru.yandex.practicum.contacts.presentation.base;
 import androidx.recyclerview.widget.DiffUtil;
 
 public class BaseListDiffCallback <T extends ListDiffInterface> extends DiffUtil.ItemCallback<T> implements ListDiffInterface<BaseListDiffCallback> {
-
     /*public boolean theSameAs(T item) {
         return this.hashCode() == item.hashCode();
     }*/
+    //Ожидал, что закомментированная функция уже проглотит BaseListDiffCallback, но нет.
+    //Дописал явную: theSameAs(BaseListDiffCallback item)
     public boolean areContentsTheSame(T item) {
         return this.equals(item);
     }
