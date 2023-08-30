36e4d8ea3068c0e6057dc6eef126292210c3e7b7
Viktoria Draenkova <vikadraenkova@yandex.by>
2023-08-30 20:59:39 +0300

    First attempt)

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/main/ContactAdapter.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/main/ContactAdapter.java
@@ -31,7 +31,6 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHold
             new AdapterListUpdateCallback(this),
             new AsyncDifferConfig.Builder<>(new BaseListDiffCallback<ContactUi>()).build()
     );
-
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
