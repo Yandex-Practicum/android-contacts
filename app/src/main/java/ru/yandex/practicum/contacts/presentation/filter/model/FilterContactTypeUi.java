692cb4420c0dd2581bfc8b45392f3b308a938406
Diifoks <dmitrus35@gmail.com>
2023-11-20 15:19:57 +0300

    Попытка№1

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/filter/model/FilterContactTypeUi.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/filter/model/FilterContactTypeUi.java
@@ -2,7 +2,8 @@ package ru.yandex.practicum.contacts.presentation.filter.model;
 
 import androidx.annotation.NonNull;
 
-public class FilterContactTypeUi {
+import ru.yandex.practicum.contacts.presentation.base.ListDiffInterface;
+public class FilterContactTypeUi implements ListDiffInterface<FilterContactTypeUi>{
 
     private final FilterContactType contactType;
     private final boolean selected;
@@ -36,5 +37,10 @@ public class FilterContactTypeUi {
         int result = contactType.hashCode();
         result = 31 * result + (selected ? 1 : 0);
         return result;
+    }
+
+    @Override
+    public boolean theSameAs(FilterContactTypeUi item) {
+        return this.getContactType() == item.getContactType();
     }
 }
