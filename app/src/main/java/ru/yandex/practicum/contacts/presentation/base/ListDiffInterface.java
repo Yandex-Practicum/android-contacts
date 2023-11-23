7f1c6573fa93da54a2716eb053b393afb77aa33e
GrafStory <grafstory@yandex.ru>
2023-11-23 19:16:49 +0300

    Добавил 2й метод equals() в интерфейс ListDiffInterface

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
@@ -2,5 +2,5 @@ package ru.yandex.practicum.contacts.presentation.base;
 
 public interface ListDiffInterface<T> {
     boolean theSameAs(T item);
-    boolean equals(Object obj);
+    boolean equals(Object o);
 }
