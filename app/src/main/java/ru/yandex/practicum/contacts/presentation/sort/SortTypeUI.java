b93bcbb16ea403b3cfa71d997837dab5b0d45bbe
AlexFM93 <Alex93FM93@gmail.com>
2024-01-09 13:46:13 +0300

    Initial and final commit and push. I hope so.))

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortTypeUI.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/sort/SortTypeUI.java
@@ -2,9 +2,10 @@ package ru.yandex.practicum.contacts.presentation.sort;
 
 import androidx.annotation.NonNull;
 
+import ru.yandex.practicum.contacts.presentation.base.ListDiffInterface;
 import ru.yandex.practicum.contacts.presentation.sort.model.SortType;
 
-public class SortTypeUI {
+public class SortTypeUI implements ListDiffInterface<SortTypeUI> {
 
     private final SortType sortType;
     private final boolean selected;
@@ -38,5 +39,10 @@ public class SortTypeUI {
         int result = sortType.hashCode();
         result = 31 * result + (selected ? 1 : 0);
         return result;
+    }
+
+    @Override
+    public boolean theSameAs(@NonNull SortTypeUI newItem) {
+        return this.getSortType() == newItem.getSortType();
     }
 }
