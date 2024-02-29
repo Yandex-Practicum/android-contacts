d7a711dd58548ad77266bc2979955990cc03950a
Joelz847 <fear.fearus@gmail.com>
2024-02-29 17:08:39 +0300

    Задача выполнена

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/main/ContactAdapter.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/main/ContactAdapter.java
@@ -94,22 +94,5 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.ViewHold
         }
     }
 
-    static class ListDiffCallback extends DiffUtil.ItemCallback<ContactUi> {
 
-        @Override
-        public boolean areItemsTheSame(@NonNull ContactUi oldItem, @NonNull ContactUi newItem) {
-            return oldItem.hashCode() == newItem.hashCode();
-        }
-
-        @Override
-        public boolean areContentsTheSame(@NonNull ContactUi oldItem, @NonNull ContactUi newItem) {
-            return oldItem.equals(newItem);
-        }
-
-        @Nullable
-        @Override
-        public Object getChangePayload(@NonNull ContactUi oldItem, @NonNull ContactUi newItem) {
-            return newItem;
-        }
-    }
 }
