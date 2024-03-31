b5757cfd6e447f89448fc9844e205c04f6a6a1cd
rr.hismatullin <hismram@gmail.com>
2024-04-01 02:44:36 +0500

    добавлен интерфейс ListDiffInterface добавлен класс BaseListDiffCallback реализующий интерфейс ListDiffInterface в классах ContactUi, FilterContactTypeUi, SortTypeUI добавлена реализация интерфейса ListDiffInterface классы ContactAdapter, FilterContactTypeAdapter, SortTypeAdapter переведены на использование BaseListDiffCallback, внутренние классы ListDiffCallback удалены

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.contacts.presentation.base;
 
 public interface ListDiffInterface<T> {
-    public boolean theSameAs(T newItem);
+    boolean theSameAs(T newItem);
 
-    public boolean equals(Object o);
+    boolean equals(Object o);
 }
--- app/src/main/java/ru/yandex/practicum/contacts/presentation/main/ContactAdapter.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/main/ContactAdapter.java
@@ -8,12 +8,10 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.AdapterListUpdateCallback;
 import androidx.recyclerview.widget.AsyncDifferConfig;
 import androidx.recyclerview.widget.AsyncListDiffer;
-import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
