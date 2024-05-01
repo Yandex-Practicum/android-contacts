98de1a0882ee74d489e79217754b220b5ed4b0a9
al2407111 <nexx2407-7@yandex.ru>
2024-05-01 15:30:36 +0300

    work2_1_05

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
@@ -1,7 +1,7 @@
 package ru.yandex.practicum.contacts.presentation.base;
 
 public interface ListDiffInterface <T>{
-    public boolean theSameAs(T item);
+    public boolean theSameAs(T t);
     public boolean equals(Object o);
 
 }
