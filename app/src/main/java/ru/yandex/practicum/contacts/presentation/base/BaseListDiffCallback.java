a6c43b580729b380f55ed5cddded80dc47476f82
Nick <55753323+OleninNick@users.noreply.github.com>
2024-05-05 18:32:02 +0400

    Изменена имплементация сравнения списков от декларирования ListDiffCallback в каждом классе, к общему дженерик классу BaseListDiffCallback

---
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.contacts.presentation.base;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+
+public class BaseListDiffCallback <T extends ListDiffInterface<T>> extends DiffUtil.ItemCallback<T> {
+    @Override
+    public boolean areItemsTheSame(@NonNull T t, @NonNull T t1) {
+        return t.theSameAs(t1);
+    }
+
+    @Override
+    public boolean areContentsTheSame(@NonNull T t, @NonNull T t1) {
+        return t.equals(t1);
+    }
+}
