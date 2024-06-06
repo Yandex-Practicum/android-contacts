a643e8e304302e5c80b1f2aa926043ec468b6473
asyakowa <muriona11@yandex.ru>
2024-06-06 05:29:00 +0300

    2 работа павловой алены

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseBottomSheetDialogFragment.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseBottomSheetDialogFragment.java
@@ -1,60 +1,44 @@
 package ru.yandex.practicum.contacts.presentation.base;
-
 import android.app.Dialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DiffUtil;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 
 import ru.yandex.practicum.contacts.databinding.FragmentBottomSheetBinding;
- interface  ListDiffInterface <T>{
-    boolean theSameAs(T item);
-    boolean equals(Object obj);
-}
- class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUtil.ItemCallback<T> {
 
-    @Override
-    public boolean areItemsTheSame(@NonNull T oldItem, @NonNull T newItem) {
-        return oldItem.theSameAs(newItem);
-    }
-
-    @Override
-    public boolean areContentsTheSame(@NonNull T oldItem, @NonNull T newItem) {
-        return oldItem.equals(newItem);
-    }
-
-    @Nullable
-    @Override
-    public Object getChangePayload(@NonNull T oldItem, @NonNull T newItem) {
-        return newItem;
-    }
-}
 public abstract class BaseBottomSheetDialogFragment<T extends BaseBottomSheetViewModel> extends BottomSheetDialogFragment {
 
     private final Class<T> viewModelClass;
 
+
+
+
+
+
+
+
+
+
     protected FragmentBottomSheetBinding binding;
     protected T viewModel;
-
     public BaseBottomSheetDialogFragment(Class<T> viewModelClass) {
         this.viewModelClass = viewModelClass;
     }
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentBottomSheetBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
-
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
@@ -69,7 +53,6 @@ public abstract class BaseBottomSheetDialogFragment<T extends BaseBottomSheetVie
         });
         return dialog;
     }
-
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -77,7 +60,6 @@ public abstract class BaseBottomSheetDialogFragment<T extends BaseBottomSheetVie
         binding.applyButton.setOnClickListener(v -> viewModel.onApplyClick());
         binding.resetButton.setOnClickListener(v -> viewModel.onResetClick());
     }
-
     @Override
     public void onDestroy() {
         binding = null;
