dffcfc679a962528853adac7bf31840512b3d045
Fantasma90 <60382015+Fantasma90@users.noreply.github.com>
2024-08-21 16:09:28 +0300

    First commit 2nd work

---
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.contacts.presentation.base;
+
+
+public interface ListDiffInterface <T>{
+
+//     boolean theSameAs(T t, Object o);
+
+     boolean theSameAs(Object o);
+
+     boolean equals(Object o);
+}
