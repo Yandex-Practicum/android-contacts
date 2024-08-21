dda129c5a21d3b135b088049c89b5b283ea3b049
Anna Frolova <an.frolova023@gmail.com>
2024-08-21 16:28:41 +0300

    Homework-Generics

---
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
@@ -0,0 +1,9 @@
+package ru.yandex.practicum.contacts.presentation.base;
+
+import androidx.annotation.NonNull;
+
+public interface ListDiffInterface<T> {
+
+    boolean theSameAs(@NonNull T t);
+    boolean equals(Object obj);
+}
