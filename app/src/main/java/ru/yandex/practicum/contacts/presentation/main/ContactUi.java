14cf3b30f2f5752d48656e249e4b3ecd89b78433
User <bagdanova.renata@gmil.com>
2024-09-22 18:45:08 +0500

    Commit

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/main/ContactUi.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/main/ContactUi.java
@@ -5,9 +5,9 @@ import androidx.annotation.NonNull;
 import java.util.List;
 
 import ru.yandex.practicum.contacts.model.ContactType;
+import ru.yandex.practicum.contacts.presentation.base.ListDiffInterface;
 
-public class ContactUi {
-
+public class ContactUi<T> implements ListDiffInterface<T> {
     private final String name;
     private final String phone;
     private final String photo;
@@ -24,6 +24,12 @@ public class ContactUi {
         this.photo = photo;
         this.types = types;
     }
+
+    @Override
+    public boolean theSameAs(T t) {
+        return types.hashCode() == t.hashCode();
+    }
+
 
     public String getName() {
         return name;
