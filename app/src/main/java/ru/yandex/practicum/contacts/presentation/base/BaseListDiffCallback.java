26582d1bd00617740a04a4e49c4b23f520845f2f
Aisylu Mezitova <amezitova@yandex.ru>
2024-10-30 23:54:13 +0300

    Выполнена задача

---
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/BaseListDiffCallback.java
@@ -0,0 +1,20 @@
+package ru.yandex.practicum.contacts.presentation.base;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+
+//public class BaseListDiffCallback<T> extends DiffUtil.ItemCallback<T> implements ListDiffInterface<BaseListDiffCallback>
+public class BaseListDiffCallback<T extends ListDiffInterface<T>> extends DiffUtil.ItemCallback<T>{
+
+    @Override
+    public boolean areItemsTheSame(@NonNull T oldItem, @NonNull T newItem) {
+        return oldItem.theSameAs(newItem);
+    }
+
+    @Override
+    public boolean areContentsTheSame(@NonNull T oldItem, @NonNull T newItem) {
+        return oldItem.equals(newItem);
+    }
+
+
+}
